c826e25505eb7e8c59b741b1de7c78b95e529662
Salc-WM <150378169+Salc-wm@users.noreply.github.com>
2025-01-31 15:22:23 -0400

    Wezterm Files
    Configs

---
+++ wezterm/wezterm/colors.lua
@@ -0,0 +1,35 @@
+do
+return {
+    -- Background colors
+    window_background = {
+        black = '#0a0a0a',
+        black_gray = '#0a0a0b',
+
+        color = '#0b0022'
+    },
+
+    -- Tab title
+    tab = {
+        edge_background = '#0b0022',
+        background = {
+            default = '#1b1032',
+            active = '#15131a',
+            hover = '#3b3052'
+        },
+        foreground = {
+            default = '#2f2e2e',
+            active = '#c0c0c0',
+            hover = '#909090'
+        }
+    },
+
+    -- Bar
+    bar = {
+        stat_color = {
+            default = '#f92a51',
+            active_key = '#0a7cbf',
+            leader_active = '#bb9af7'
+        }, foreground_item = '#f19c21'
+    }
+}
+end
---
+++ wezterm/wezterm/foo.lua
@@ -0,0 +1,17 @@
+return {
+    basename = function(s)
+        return string.gsub(s, '(.*[/\\])(.*)', '%2')
+    end,
+
+    Tab_title = function(tab_info)
+        local title = tab_info.tab_title
+
+        -- if the tab title is explicitly set, take that
+        if title and #title > 0 then
+            return title
+        end
+
+        -- Otherwise, use the title from the active pane
+        return tab_info.active_pane.title  -- in that tab
+    end
+}
